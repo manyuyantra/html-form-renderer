e5f9c7255ce6c9de98c1e4f82262240c88395369
gurujegan <gurujegan.nathan@gmail.com>
2021-04-14 21:21:32 +0530

    constructor check

---
+++ pages/todos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="123">
+    <todos />
+    <p v-if="$fetchState.pending">Loading...</p>
+    {{ customer }}
+  </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      customer: null
+    }
+  },
+  beforeMount () {
+    function Customer () {
+      this.name = ''
+      this.age = ''
+      this.gender = []
+    }
+    this.customer = new Customer()
+  },
+  mounted () {
+    console.log(this.customer)
+  },
+  async fetch () {
+    this.customer = 'dfdf'
+    await Promise.resolve(10)
+  }
+}
+</script>
